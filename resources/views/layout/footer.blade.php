14dc26b52ebb8f47cf1ed82d573899d671115809
Raul Tenorio <raul.tenorio@finantech.com.ec>
2022-05-30 22:24:16 -0500

    footer update

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,5 @@
-(5 sloc)  142 Bytes
-   
-   <footer class="footer bg-dark text-center">
-       <div class="container ">
-           Todos los derechos reservados @BYRONTOSH
-       </div>
-   </footer>
+<footer class="footer bg-dark text-center">
+    <div class="container ">
+        Todos los derechos reservados @BYRONTOSH
+    </div>
+</footer>
